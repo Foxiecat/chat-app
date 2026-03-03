cee515a6c7d697d7290893973f4e5fdbd472f0a7
Foxiecat <foxiecat+programming@pm.me>
2026-03-03 13:17:29 +0100

    Fixed small mistakes

--- Database/init.sql
+++ Database/init.sql
@@ -1,24 +1,20 @@
-CREATE EXTENSION if not exists "pgcrypto";
-
-GRANT ALL ON SCHEMA public TO admin;
-
 -- Member
 CREATE TABLE Member (
-    Id UUID NOT NULL DEFAULT PRIMARY KEY,
+    Id UUID NOT NULL PRIMARY KEY,
     Username VARCHAR(10) NOT NULL UNIQUE,
     PasswordHash TEXT NOT NULL,
     Status VARCHAR(20) NOT NULL DEFAULT 'offline',
     Created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    Updated TIMESTAMPTZ,
+    Updated TIMESTAMPTZ
 );
 
 
 -- Community
 CREATE TABLE Community (
-    Id UUID NOT NULL DEFAULT PRIMARY KEY,
+    Id UUID NOT NULL PRIMARY KEY,
     OwnerId UUID NOT NULL,
     Name VARCHAR(30) NOT NULL,
-    Created TIMESTAMPTZ NOT NULL,
+    Created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     Updated TIMESTAMPTZ,
 
     FOREIGN KEY (OwnerId) REFERENCES Member(Id)
@@ -32,13 +28,13 @@ CREATE TABLE CommunityMember (
 
     PRIMARY KEY (MemberId, CommunityId),
     FOREIGN KEY (MemberId) REFERENCES Member(Id) ON DELETE CASCADE,
-    FOREIGN KEY (CommunityId) REFERENCES Community ON DELETE CASCADE,
+    FOREIGN KEY (CommunityId) REFERENCES Community(Id) ON DELETE CASCADE
 );
 
 
 -- Channel
 CREATE TABLE Channel (
-    Id UUID NOT NULL DEFAULT PRIMARY KEY,
+    Id UUID NOT NULL PRIMARY KEY,
     CommunityId UUID NOT NULL,
     Name VARCHAR(30) NOT NULL,
     Position INT NOT NULL DEFAULT 0,
@@ -51,8 +47,8 @@ CREATE TABLE Channel (
 
 -- Message
 CREATE TABLE Message (
-    Id UUID NOT NULL DEFAULT PRIMARY KEY,
-    ChannelId UUI NOT NULL,
+    Id UUID NOT NULL PRIMARY KEY,
+    ChannelId UUID NOT NULL,
     SenderId UUID NOT NULL,
     Content TEXT NOT NULL,
     IsDeleted BOOLEAN NOT NULL DEFAULT FALSE,
@@ -60,7 +56,7 @@ CREATE TABLE Message (
     Updated TIMESTAMPTZ,
 
     FOREIGN KEY (ChannelId) REFERENCES Channel(Id) ON DELETE CASCADE,
-    FOREIGN KEY (SenderId) REFERENCES Member(Id),
+    FOREIGN KEY (SenderId) REFERENCES Member(Id)
 );
 
 
