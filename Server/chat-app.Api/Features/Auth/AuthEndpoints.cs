b82ee22e9e8b8a9c995475d654bdcb72f792dc0e
Foxiecat <foxiecat+programming@pm.me>
2026-03-12 12:56:47 +0100

    Added reminder

--- Server/chat-app.Api/Features/Auth/AuthEndpoints.cs
+++ Server/chat-app.Api/Features/Auth/AuthEndpoints.cs
@@ -1,9 +1,4 @@
-using chat_app.Api.Database;
-using chat_app.Api.Features.Auth.DTOs;
 using chat_app.Api.Features.Auth.Endpoints;
-using chat_app.Api.Features.Auth.Interfaces;
-using chat_app.Api.Features.Members;
-using Microsoft.EntityFrameworkCore;
 
 namespace chat_app.Api.Features.Auth;
 
@@ -11,45 +6,12 @@ public class AuthEndpoints(
     RegisterService registerService,
     LoginService loginService)
 {
+    // TODO: Map the Endpoints in program.cs
     public void MapAuthEndpoints(WebApplication app)
     {
         var authGroup = app.MapGroup("/auth");
 
         authGroup.MapPost("/register", registerService.HandleAsync).WithName("Register").AllowAnonymous();
         authGroup.MapPost("/login", loginService.HandleAsync).WithName("Login").AllowAnonymous();
-    }
-
-    private static async Task<IResult> RegisterAsync(
-        ChatDbContext dbContext,
-        ITokenService tokenService,
-        RegisterRequest request
-    )
-    {
-        if (
-            string.IsNullOrWhiteSpace(request.Username)
-            || string.IsNullOrWhiteSpace(request.Password)
-        )
-        {
-            return Results.BadRequest("Username and password are required.");
-        }
-
-        var exists = await dbContext.Member.AnyAsync(member => member.Username == request.Username);
-        if (exists)
-            return Results.Conflict("Username already taken!");
-
-        Member member = new()
-        {
-            Id = Guid.NewGuid(),
-            Username = request.Username,
-            Status = "offline",
-            PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(request.Password),
-            Created = DateTime.UtcNow,
-        };
-
-        dbContext.Member.Add(member);
-        await dbContext.SaveChangesAsync();
-
-        AuthResponse response = new(tokenService.GenerateToken(member), member.Id, member.Username);
-        return Results.Ok(response);
     }
 }
